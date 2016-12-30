c7486bd1732090e6613b5afaf89e68b9e679c71f
FreeStar <1064389584@qq.com>
2016-12-30 16:36:00 +0800

    1
    准备模仿写BGA的图片选择器

---
+++ app/src/main/java/freestar/freephotopicket/FreePhotoPickerActivity.java
@@ -0,0 +1,14 @@
+package freestar.freephotopicket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FreePhotoPickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_free_photo_picker);
+
+    }
+}
